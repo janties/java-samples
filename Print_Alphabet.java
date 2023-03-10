c96db27e249f9b1d9e54785c6d838786264109c2
janties <com.kerem@gmail.com>
2023-03-10 20:11:26 +0300

    ex

--- Print_Alphabet.java
+++
@@ -1,9 +0,0 @@
-// Prints Japanese Katakana Alphabet if your pc supports that unicode
-public class Print_Alphabet
-{
-    public static void main(String[] args)
-    {
-        for(char c = 0x30A0; c <= 0x30FF; c++)
-            System.out.print(c+ ", ");
-    }
-}
--- Print_params.java
+++
@@ -1,9 +0,0 @@
-public class Print_params
-{
-    public static void main(String[] args)
-    {
-        int i = 0;
-        while(i < args.length)
-            System.out.println(args[i++]);
-    }
-}
