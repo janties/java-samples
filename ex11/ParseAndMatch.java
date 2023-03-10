6195a1da07805d5e603a81475100175c344e0cd0
janties <com.kerem@gmail.com>
2023-03-10 20:11:55 +0300

    ex

---
+++ ex11/ParseAndMatch.java
@@ -0,0 +1,13 @@
+public class ParseAndMatch
+{
+    public static void main(String[] args)
+    {
+        double s = 0;
+        for(String x: args)
+        {
+            if(x.matches("[0-9\\.]+"))// ???
+                s += Double.parseDouble(x);
+        }
+        System.out.println("Sum is "+s);
+    }
+}
