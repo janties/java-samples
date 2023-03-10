f7785f49e31af7e706323bd87ee28ccfaad91fa2
janties <com.kerem@gmail.com>
2023-03-10 20:01:47 +0300

    ex

---
+++ Print_params.java
@@ -0,0 +1,9 @@
+public class Print_params
+{
+    public static void main(String[] args)
+    {
+        int i = 0;
+        while(i < args.length)
+            System.out.println(args[i++]);
+    }
+}
